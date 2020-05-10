51f33641accccadd5ea733e059456ef5dbbc84e4
UnlimatedStone9 <40124890+UnlimatedStone9@users.noreply.github.com>
2020-05-10 16:11:44 +0100

    Modify mod ID/ mod name for TFE Fork

--- src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
+++ src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
@@ -13,8 +13,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = TFC_RocksPlus.MODID, name = TFC_RocksPlus.NAME, version = TFC_RocksPlus.VERSION)
 public class TFC_RocksPlus
 {
-    public static final String MODID = "tfc_rocksplus";
-    public static final String NAME = "TFC Rocks+";
+    public static final String MODID = "tfe_tfc_rocksplus";
+    public static final String NAME = "TFC Rocks+ TFE Fork";
     public static final String VERSION = "1.2";
 
     private static Logger logger;
