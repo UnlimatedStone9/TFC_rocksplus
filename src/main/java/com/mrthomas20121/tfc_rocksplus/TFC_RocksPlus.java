a8b9d54723bc5d8eb40c4eb989662ca3569b23e6
UnlimatedStone9 <40124890+UnlimatedStone9@users.noreply.github.com>
2020-05-28 19:59:08 +0100

    Update TFC_RocksPlus.java

--- src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
+++ src/main/java/com/mrthomas20121/tfc_rocksplus/TFC_RocksPlus.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class TFE_TFC_RocksPlus
 {
     public static final String MODID = "tfe_tfc_rocksplus";
-    public static final String NAME = "TFC Rocks+ TFE Fork";
+    public static final String NAME = "TFE - TFC Rocks+";
     public static final String VERSION = "1.2";
 
     private static Logger logger;
